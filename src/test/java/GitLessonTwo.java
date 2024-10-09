d5af15a23ecc5341b095c836e9700671eb5e8060
Павел Королев <99354950+PavelKorolevA@users.noreply.github.com>
2024-10-09 22:31:13 +0300

    Update GitLessonTwo.java

--- src/test/java/GitLessonTwo.java
+++ src/test/java/GitLessonTwo.java
@@ -1,9 +1,10 @@
 public class GitLessonTwo {
+    private tatic String omg = "Merge conflict!!!!"
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
         int c = a + b;
-        System.out.println("The sum of a and b is " + c);
+        System.out.println("The sum of a and b is " + c + " " + "Hello, Pitter Parker");
     }
 
 }
