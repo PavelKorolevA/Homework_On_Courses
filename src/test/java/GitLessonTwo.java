1c7280e41964ffb4c0e84673c605858ebcdd0de1
Pavel Korolev <amlerok96@gmail.com>
2024-10-09 22:17:50 +0300

    sum a+b

--- src/test/java/GitLessonTwo.java
+++ src/test/java/GitLessonTwo.java
@@ -1,6 +1,9 @@
 public class GitLessonTwo {
     public static void main(String[] args) {
-        System.out.println("Hello My Friends");
+        int a = 10;
+        int b = 20;
+        int c = a + b;
+        System.out.println("The sum of a and b is " + c);
     }
 
 }
