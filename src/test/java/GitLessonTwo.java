48c912c0f8021ea044b2cf18d4f588bc9043e11b
Pavel Korolev <amlerok96@gmail.com>
2024-10-09 22:49:08 +0300

    added many classes two

--- src/test/java/GitLessonTwo.java
+++ src/test/java/GitLessonTwo.java
@@ -1,7 +1,8 @@
 public class GitLessonTwo {
     private static String omg = "Merge conflict!!!!";
     private static String ufc = "Konor vs McFregor";
-    
+
+
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
@@ -27,5 +28,31 @@ public class GitLessonTwo {
 
         }
         return result;
+    }
+
+    public static int factorial(int n) {
+        int result = 1;
+        for (int i = 1; i <= n; i++) {
+            result *= i;
+        }
+        return result;
+    }
+
+    public static int fibonacci(int n) {
+        if (n == 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+    }
+
+    public static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
     }
 }
