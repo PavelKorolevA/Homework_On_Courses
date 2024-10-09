7be8561688aee7e658c6f857620b35385aa26ad7
Pavel Korolev <amlerok96@gmail.com>
2024-10-09 22:30:09 +0300

    added many classes

--- src/test/java/GitLessonTwo.java
+++ src/test/java/GitLessonTwo.java
@@ -6,4 +6,32 @@ public class GitLessonTwo {
         System.out.println("The sum of a and b is " + c);
     }
 
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static int divide(int a, int b) {
+        return a / b;
+    }
+
+    public static int modulo(int a, int b) {
+        return a % b;
+    }
+
+    public static int power(int a, int b) {
+        int result = 1;
+        for (int i = 0; i < b; i++) {
+            result *= a;
+
+        }
+        return result;
+    }
 }
