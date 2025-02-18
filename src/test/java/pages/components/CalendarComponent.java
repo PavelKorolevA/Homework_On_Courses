4a6f7038e084d9c0ca20d9e24603f709704562af
https://github.com/PavelKorolevA/Homework_On_Courses <https://github.com/PavelKorolevA@Homework_On_Courses>
2025-02-18 13:41:25 +0300

    Add fix CalendarComponent

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -1,13 +1,16 @@
 package pages.components;
 import pages.DemoQaPage;
 
+import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CalendarComponent {
     public void setDate() {
-        $("#dateOfBirthInput");
-        $(".react-datepicker__month-select");
-        $(".react-datepicker__year-select");
-        $(".react-datepicker__day react-datepicker__day--019");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").click();
+        $$("[value=\"10\"]").findBy(text("November")).click();
+        $(".react-datepicker__year-select").click();
+        $$("[value=\"1996\"]").findBy(text("1996")).click();
+        $(".react-datepicker__day--019").click();
     }
 }
--- src/test/java/tests/shorttest/DemoQaTest.java
+++ src/test/java/tests/shorttest/DemoQaTest.java
@@ -1,11 +1,17 @@
 package tests.shorttest;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import pages.DemoQaPage;
 
 class DemoQaTest extends DemoQaPage {
-
     DemoQaPage demoQaPage = new DemoQaPage();
+    @BeforeAll
+    static void setUp() {
+        Configuration.browserSize = "1920x1080";
+        Configuration.pageLoadStrategy = "eager";
+    }
 
     @Test
     void testDemoQa() {
