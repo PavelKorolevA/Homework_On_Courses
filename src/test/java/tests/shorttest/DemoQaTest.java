7c751fafa602fc212a6001c78f0346cedded5682
https://github.com/PavelKorolevA/Homework_On_Courses <https://github.com/PavelKorolevA@Homework_On_Courses>
2025-04-08 12:23:04 +0300

    Наследование

--- src/test/java/tests/shorttest/DemoQaTest.java
+++ src/test/java/tests/shorttest/DemoQaTest.java
@@ -1,7 +1,5 @@
 package tests.shorttest;
 
-import com.codeborne.selenide.Configuration;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
